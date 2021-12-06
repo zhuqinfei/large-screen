70b7ce031efc6485692a3319bd3a7ee6db438fbe
zhuqinfei <1586859762@qq.com>
2021-12-06 18:03:10 +0800

    创建路由并初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import {createWebHashHistory,createRouter} from 'vue-router'
+import Home from './components/Home.vue'
 
-createApp(App).mount('#app')
+const history=createWebHashHistory()
+const router=createRouter({
+    history:history,
+    routes:[
+        {path:'/',component:Home}  //路由路径和组件
+    ]
+})
+const app=createApp(App)
+app.use(router)
+app.mount('#app')
