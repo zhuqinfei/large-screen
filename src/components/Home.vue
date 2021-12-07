ed350b83a63bf96cb43241b5a5f3fc794094b2f2
zhuqinfei <1586859762@qq.com>
2021-12-07 17:32:32 +0800

    add px function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>hi</div>
+    <div>
+        <div class="x"></div>
+    </div>
 </template>
+
+<style lang='scss'>
+@import "../shared/helper";
+.x {
+  width: px(367);
+  height: px(315);
+  border: 1px solid red;
+}
+</style>
