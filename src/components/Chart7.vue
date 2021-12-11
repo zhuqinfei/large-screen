fb1aa8e17ebdea5487cec8a5724d4b1d1645ccc6
zhuqinfei <1586859762@qq.com>
2021-12-12 01:32:05 +0800

    Chart7 完成

--- src/components/Chart7.vue
+++ src/components/Chart7.vue
@@ -1,7 +1,57 @@
 <template>
- <div className="年龄段-图1">7
-      <div id="chart7" className="chart">
-
-      </div>
+    <div className="年龄段-图1">
+        <div className="chart">
+            <div className="main" id="chart7"></div>
+            <div className="text">性别</div>
+        </div>
+        <div className="legend">
+            <span className="male" />男
+            <span className="female" />女
+        </div>
     </div>
 </template>
+
+<script lang="ts">
+import * as echarts from 'echarts';
+import { onMounted } from 'vue';
+import { baseEchartOptions } from '../shared/base-echart-options';
+import { px } from '../shared/px';
+
+export default {
+    setup() {
+        onMounted(() => {
+            let myChart = echarts.init(document.getElementById("chart7"));
+            myChart.setOption({
+                 ...baseEchartOptions,
+                color: ['#8D70F8', '#33A4FA'],
+                xAxis: { show: false },
+                yAxis: { show: false },
+                legend: { show: false },
+                series: [
+                    {
+                        name: '访问来源',
+                        type: 'pie',
+                        radius: ['75%', '90%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                            show: true, position: 'inside', textStyle: { color: 'white', fontSize: px(20) },
+                            formatter(options) {
+                                return options.value * 100 + '%';
+                            }
+                        },
+                        labelLine: { show: false },
+                        itemStyle: {
+                            borderColor: '#0F113A',
+                            borderWidth: px(4)
+                        },
+                        data: [
+                            { value: 0.2, name: '女' },
+                            { value: 0.8, name: '男' },
+                        ]
+                    }
+                ]
+            });
+        })
+    }
+}
+</script>
